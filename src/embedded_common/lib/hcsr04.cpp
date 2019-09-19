919fea979f0603cf8cf15af75a02584484d6475a
ruhlmcu <curt@ruhlman.us>
2019-09-19 15:32:19 -0500

    Update hcsr04.cpp

--- src/embedded_common/lib/hcsr04.cpp
+++ src/embedded_common/lib/hcsr04.cpp
@@ -8,11 +8,6 @@
  * Elsewhere we could handle the conversion from the float to a boolean based on it being greater than or equal to a range.
  */
 
-#if ARDUINO >= 100
-  #include <Arduino.h>
-#else
-  #include <WProgram.h>
-#endif
 #include "hcsr04.h"
 #include "OmniThing.h"
 #include "Logger.h"
