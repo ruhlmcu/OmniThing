cec7c4c74c7fcca17e587c8fe13744ff4fbdacac
ruhlmcu <curt@ruhlman.us>
2019-09-25 08:03:00 -0500

    Update OmniThing.cpp

--- src/core/OmniThing.cpp
+++ src/core/OmniThing.cpp
@@ -919,6 +919,7 @@ namespace omni
             for(unsigned int i = 0; i < m_CompositePeriphConfigs.getCount(); ++i)
             {
                 auto conf = m_CompositePeriphConfigs[i];
+                LOG << F("\t") << i << F("\n");
                 LOG << F("\t") << m_CompositePeriphConfigs[i]->getType() << F("\n");
                 LOG << F("\t") << buffer << F("\n");
                 LOG << F("\t") << m_CompositePeriphConfigs[i] << F("\n");
