15bc1d1135d3ee27ae71ec125bcc5b17ea76f2d6
ruhlmcu <curt@ruhlman.us>
2019-09-25 11:06:12 -0500

    update

--- example_configs/ArduinoJsonConfig.h
+++ example_configs/ArduinoJsonConfig.h
@@ -8,19 +8,19 @@ namespace omni
 const char NetworkReceiver[] PROGMEM =
 R"RAWSTR({"NetworkReceiver":
 {
-    "password": null, 
-    "type": "NetworkManagerEsp8266", 
-    "port": 1337, 
+    "password": null,
+    "type": "NetworkManagerEsp8266",
+    "port": 1337,
     "ssid": null
 }})RAWSTR";
 
 const char NetworkSender[] PROGMEM =
 R"RAWSTR({"NetworkSender":
 {
-    "ip": "192.168.1.30", 
-    "password": "Toyota3879", 
-    "type": "NetworkManagerEsp8266", 
-    "port": 39500, 
+    "ip": "192.168.1.30",
+    "password": "Toyota3879",
+    "type": "NetworkManagerEsp8266",
+    "port": 39500,
     "ssid": "ruhlman"
 }})RAWSTR";
 
@@ -28,12 +28,13 @@ const char CompositePeriphs[] PROGMEM =
 R"RAWSTR({"CompositePeriphs":
 [
     {
-        "triggerPin": 12, 
-        "name": "Garage Sensor 01", 
+        "triggerPin": 12,
+        "type": "DistanceSensor",
+        "name": "Garage Sensor 01",
         "echoPin": 13, 
-        "invert": true, 
-        "pullup": true, 
-        "type": "DistanceSensor"
+        "invert": true,
+        "pullup": true
+
     }
 ]})RAWSTR";
 
