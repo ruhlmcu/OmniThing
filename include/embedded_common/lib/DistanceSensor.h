4fff4e550f0d9cd1ca4f997478e0936807e5344f
ruhlmcu <curt@ruhlman.us>
2019-09-25 15:00:36 -0500

    moving from a composite peripheral to a type

--- include/embedded_common/lib/DistanceSensor.h
+++ include/embedded_common/lib/DistanceSensor.h
@@ -1,39 +1,75 @@
-/*
- * A port of Library for hcsr04 Ranging Module in a minimalist way
- * created 3 Apr 2014 by Erick Simões (github: @ErickSimoes | twitter: @AloErickSimoes)
- */
+// A port of Rob Tillaart's Arduino DHT library to OmniThing
+// https://github.com/RobTillaart/Arduino/tree/master/libraries/DHTstable
 
-#ifndef OMNI_DISTANCESENSOR_H
-#define OMNI_DISTANCESENSOR_H
-
+#ifndef OMNI_DHTREADER_H
+#define OMNI_DHTREADER_H
 
 #include "CompositePeripheral.h"
 #include "ObjectConfig.h"
-#include "InputUInt.h"
-#include "InputFloat.h"
+#include "DigitalInputPin.h"
+#include "DigitalOutputPin.h"
 
-/*
- * Values of divisors
- */
-#define CM 28
-#define INC 71
+#define DHTLIB_OK               0
+#define DHTLIB_ERROR_CHECKSUM  -1
+#define DHTLIB_ERROR_TIMEOUT   -2
+#define DHTLIB_INVALID_VALUE   -999
+
+#define DHTLIB_DHT11_WAKEUP     18
+#define DHTLIB_DHT_WAKEUP       1
+
+#define DHTLIB_TIMEOUT          100 // (microseconds)
+
+#define MIN_DHT_POLL_MS         2000
+
 namespace omni
 {
-class DistanceSensor : public InputUInt, public InputFloat
-{
-  private:
-    uint8_t trig;
-    uint8_t echo;
-    unsigned long previousMicros;
-    unsigned long timeout;
-    unsigned int timing();
+    class DhtReader : public CompositePeripheral
+    {
+        private:
+            unsigned char bits[5]; //buffer to receive data
+            float humidity;
+            float temperature;
 
-  public:
-    DistanceSensor(uint8_t trigPin, uint8_t echoPin, unsigned long timeOut = 20000UL);
-    unsigned int read(uint8_t und = CM);
-    void setTimeout(unsigned long timeOut) {timeout = timeOut;}
+            float last_humidity;
+            float last_temperature;
 
+            unsigned long long poll_timer;
 
-};
+            unsigned short pin;
+            bool is_dht11;
+            bool internal_pullup;
+            DigitalInputPin* inputPin;
+            DigitalOutputPin* outputPin;
+
+            unsigned int word(unsigned char h, unsigned char l);
+
+            int _readSensor(unsigned char wakeupDelay);
+
+            int read11();
+            int read();
+
+            int read21() {return read();}
+            int read22() {return read();}
+            int read33() {return read();}
+            int read44() {return read();}
+
+            int getVals();
+
+        public:
+            DhtReader(const char* name, unsigned short pinNum, bool dht11, bool pullup);
+            virtual ~DhtReader();
+
+            virtual bool getFloat(const char* name, float& f);
+
+            static CompositePeripheral* createFromJson(const char* json);
+
+            //params
+            static const char* Param_Temperature;
+            static const char* Param_Humidity;
+
+            static const char* Type;
+
+            static ObjectConfig<CompositePeripheral> PeriphConf;
+    };
 }
 #endif
--- src/embedded_common/lib/DistanceSensor.cpp
+++ src/embedded_common/lib/DistanceSensor.cpp
@@ -1,56 +1,238 @@
-/*
- * A port of Library for hcsr04 Ranging Module in a minimalist way
- * created 3 Apr 2014 by Erick Simões (github: @ErickSimoes | twitter: @AloErickSimoes)
- */
- /*
- * For ESP support, we just need to add a new file to src/embedded_common/lib called something like hcsr04.cpp .
- * It would derive from the class called InputFloat and would return the measured distance in its getFloat() method.
- * Elsewhere we could handle the conversion from the float to a boolean based on it being greater than or equal to a range.
- */
+// A port of Rob Tillaart's Arduino DHT library to OmniThing
+// https://github.com/RobTillaart/Arduino/tree/master/libraries/DHTstable
 
-#include "DistanceSensor.h"
-
+#include "DhtReader.h"
 #include "OmniThing.h"
 #include "Logger.h"
 #include "OmniUtil.h"
-//#include <Arduino.h>
+
 #include "frozen.h"
 #include <string.h>
 
 namespace omni
 {
+//private
+    unsigned int DhtReader::word(unsigned char h, unsigned char l)
+    {
+        return ( ( (int) h ) << 8 ) | l;
+    }
 
-DistanceSensor::DistanceSensor(uint8_t trigPin, uint8_t echoPin, unsigned long timeOut) {
-  trig = trigPin;
-  echo = echoPin;
-  pinMode(trig, OUTPUT);
-  pinMode(echo, INPUT);
-  timeout = timeOut;
-}
+    int DhtReader::_readSensor(unsigned char wakeupDelay)
+    {
+        // INIT BUFFERVAR TO RECEIVE DATA
+        unsigned char mask = 128;
+        unsigned char idx = 0;
 
-unsigned int DistanceSensor::timing() {
+        // EMPTY BUFFER
+        for (unsigned char i = 0; i < 5; i++) bits[i] = 0;
 
-  digitalWrite(trig, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig, LOW);
+        // REQUEST SAMPLE
+        outputPin->configure();
+        outputPin->writeBool(false);
+        sleepMillisBusy(wakeupDelay);
+        inputPin->configure();
+        inputPin->readBool();
+        sleepMicrosBusy(40);
 
-  previousMicros = micros();
-  while(!digitalRead(echo) && (micros() - previousMicros) <= timeout); // wait for the echo pin HIGH or timeout
-  previousMicros = micros();
-  while(digitalRead(echo)  && (micros() - previousMicros) <= timeout); // wait for the echo pin LOW or timeout
+        unsigned long long timer;
+        // GET ACKNOWLEDGE or TIMEOUT
+        timer = getMicros();
+        while(inputPin->readBool() == false)
+        {
+            if (getMicros() - timer >= DHTLIB_TIMEOUT) return DHTLIB_ERROR_TIMEOUT;
+        }
 
-  return micros() - previousMicros; // duration
-}
+        timer = getMicros();
+        while(inputPin->readBool() == true)
+        {
+            if (getMicros() - timer >= DHTLIB_TIMEOUT) return DHTLIB_ERROR_TIMEOUT;
+        }
 
-/*
- * If the unit of measure is not passed as a parameter,
- * by default, it will return the distance in centimeters.
- * To change the default, replace CM by INC.
- */
-unsigned int DistanceSensor::read(uint8_t und) {
-  LOG << F("Made it to here") << timing()/ und / 2 << F(")\n");
-  return timing() / und / 2;  //distance by divisor
-}
+        // READ THE OUTPUT - 40 BITS => 5 BYTES
+        for (unsigned char i = 40; i != 0; i--)
+        {
+            timer = getMicros();
+            while(inputPin->readBool() == false)
+            {
+                if (getMicros() - timer >= DHTLIB_TIMEOUT) return DHTLIB_ERROR_TIMEOUT;
+            }
+
+            unsigned long long t = getMicros();
+
+            timer = getMicros();
+            while(inputPin->readBool() == true)
+            {
+                if (getMicros() - timer >= DHTLIB_TIMEOUT) return DHTLIB_ERROR_TIMEOUT;
+            }
+
+            if ((getMicros() - t) > 40)
+            {
+                bits[idx] |= mask;
+            }
+            mask >>= 1;
+            if (mask == 0)   // next byte?
+            {
+                mask = 128;
+                idx++;
+            }
+        }
+
+        return DHTLIB_OK;
+    }
+
+    int DhtReader::read11()
+    {
+        // READ VALUES
+        int rv = _readSensor(DHTLIB_DHT11_WAKEUP);
+        if (rv != DHTLIB_OK)
+        {
+            humidity    = DHTLIB_INVALID_VALUE; // invalid value, or is NaN prefered?
+            temperature = DHTLIB_INVALID_VALUE; // invalid value
+            return rv;
+        }
+
+        // CONVERT AND STORE
+        humidity    = bits[0];  // bits[1] == 0;
+        temperature = bits[2];  // bits[3] == 0;
+
+        // TEST CHECKSUM
+        unsigned char sum = bits[0] + bits[1] + bits[2] + bits[3];
+        if (bits[4] != sum)
+        {
+          return DHTLIB_ERROR_CHECKSUM;
+        }
+        return DHTLIB_OK;
+    }
+
+    int DhtReader::read()
+    {
+        // READ VALUES
+        int rv = _readSensor(DHTLIB_DHT_WAKEUP);
+        if (rv != DHTLIB_OK)
+        {
+            humidity    = DHTLIB_INVALID_VALUE;  // invalid value, or is NaN prefered?
+            temperature = DHTLIB_INVALID_VALUE;  // invalid value
+            return rv; // propagate error value
+        }
+
+        // CONVERT AND STORE
+        humidity = word(bits[0], bits[1]) * 0.1;
+        temperature = word(bits[2] & 0x7F, bits[3]) * 0.1;
+        if (bits[2] & 0x80)  // negative temperature
+        {
+            temperature = -temperature;
+        }
+
+        // TEST CHECKSUM
+        unsigned char sum = bits[0] + bits[1] + bits[2] + bits[3];
+        if (bits[4] != sum)
+        {
+            return DHTLIB_ERROR_CHECKSUM;
+        }
+        return DHTLIB_OK;
+    }
+
+    int DhtReader::getVals()
+    {
+        if(getMillis() - poll_timer >= MIN_DHT_POLL_MS)
+        {
+            poll_timer = getMillis();
+        }
+        else
+        {
+            return DHTLIB_OK;
+        }
+
+        int ret;
+        if(is_dht11)
+            ret = read11();
+        else
+            ret = read();
+
+        if(ret != DHTLIB_OK)
+        {
+            humidity = last_humidity;
+            temperature = last_temperature;
+            LOG << F("ERROR: error reading temp/humidity (") << ret << F(")\n");
+        }
+        else
+        {
+            last_humidity = humidity;
+            last_temperature = temperature;
+        }
+
+        return ret;
+    }
+
+//public
+    DhtReader::DhtReader(const char* name, unsigned short pinNum, bool dht11, bool pullup):
+        CompositePeripheral(name),
+        poll_timer(getMillis() - MIN_DHT_POLL_MS),
+        pin(pinNum),
+        is_dht11(dht11),
+        internal_pullup(pullup),
+        inputPin(DigitalInputPin::create(pin, false, internal_pullup)),
+        outputPin(DigitalOutputPin::create(pin, true, false))
+    {
+
+    }
+
+    DhtReader::~DhtReader()
+    {
+
+    }
+
+    bool DhtReader::getFloat(const char* name, float& f)
+    {
+        bool ret;
+        if(!strcmp(name, Param_Temperature))
+        {
+            ret = (getVals() == DHTLIB_OK);
+            f = temperature;
+            return ret;
+        }
+        else if(!strcmp(name, Param_Humidity))
+        {
+            ret = (getVals() == DHTLIB_OK);
+            f = humidity;
+            return ret;
+        }
+        else
+        {
+            LOG << F("ERROR: ") << name << F(" is not a param of ") << Type << F("\n");
+            f = DHTLIB_INVALID_VALUE;
+            return false;
+        }
+    }
+
+    CompositePeripheral* DhtReader::createFromJson(const char* json)
+    {
+        const char* name = parseName(json);
+        if(!name)
+            return nullptr;
+
+        unsigned int len = strlen(json);
+        unsigned short pin;
+        bool dht11 = false;
+        bool pullup = false;
+
+        if(json_scanf(json, len, "{pin: %hu}", &pin) != 1)
+        {
+            LOG << F("ERROR: pin number required\n");
+            return nullptr;
+        }
+
+        json_scanf(json, len, "{dht11: %B}", &dht11);
+        json_scanf(json, len, "{pullup: %B}", &pullup);
+
+        return new DhtReader(name, pin, dht11, pullup);
+    }
+
+    //statics
+    const char* DhtReader::Param_Temperature = "temperature";
+    const char* DhtReader::Param_Humidity    = "humidity";
+
+    const char* DhtReader::Type = "DhtReader";
+
+    ObjectConfig<CompositePeripheral> DhtReader::PeriphConf(Type, createFromJson);
 }
