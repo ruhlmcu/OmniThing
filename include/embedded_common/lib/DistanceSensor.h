b9b3a8c7df088741f6e6a2e8da2bac33b7f821c5
ruhlmcu <curt@ruhlman.us>
2019-09-26 14:30:49 -0500

    update

--- include/embedded_common/lib/DistanceSensor.h
+++ include/embedded_common/lib/DistanceSensor.h
@@ -15,25 +15,52 @@
 
 namespace omni
 {
-    class distanceSensor : public DigitalOutputPin, public DigitalInputPin
-    {
-        private:
+  class distanceSensor : public DigitalOutputPin, public DigitalInputPin
+  {
+  private:
+//private: from DigitalxOutputPinArduino and DigitalxInputPinArduino
+    static distanceSensor* createFromJson(const char* json);
+    bool m_bPullup;
+    //private: from Ultrasonic Library Example
+    uint8_t trig;
+    uint8_t echo;
+    bool threePins = false;
+    unsigned long previousMicros;
+    unsigned long timeout;
+    unsigned int timing();
+//protected: from DigitalxOutputPinArduino and DigitalxInputPinArduino
+  protected:
+    void writePin(bool b) final;
+    bool readPin() final;
+  public:
+//public: from distanceSensor and DigitalxInputPinArduino
+    distanceSensor(unsigned short pin, bool initialVal, bool invertLogic);
 
-//          private: from Ultrasonic Library Example
-            uint8_t trig;
-            uint8_t echo;
-            bool threePins = false;
-            unsigned long previousMicros;
-            unsigned long timeout;
-            unsigned int timing();
+    virtual ~distanceSensor();
 
-        public:
+    virtual bool configure();
 
-//          public: from Ultrasonic Library Example
-            distanceSensor(uint8_t sigPin) : distanceSensor(sigPin, sigPin) {};
-            distanceSensor(uint8_t triggerPin, uint8_t echoPin, unsigned long timeOut = 20000UL);
-            unsigned int read(uint8_t und = CM);
-            void setTimeout(unsigned long timeOut) {timeout = timeOut;}
-    };
+    static OutputVoid* createVoidFromJson(const char* json);
+    static OutputBool* createBoolFromJson(const char* json);
+
+    static const char* Type;
+    static ObjectConfig<OutputVoid> OutputVoidConf;
+    static ObjectConfig<OutputBool> OutputBoolConf;
+    distanceSensor(unsigned short pin, bool invertLogic, bool pullup);
+
+    virtual ~distanceSensor();
+
+    virtual bool configure();
+
+    static InputBool* createFromJson(const char* json);
+
+    static const char* Type;
+    static ObjectConfig<InputBool> InputBoolConf;
+//public: from Ultrasonic Library Example
+    distanceSensor(uint8_t sigPin) : distanceSensor(sigPin, sigPin) {};
+    distanceSensor(uint8_t triggerPin, uint8_t echoPin, unsigned long timeOut = 20000UL);
+    unsigned int read(uint8_t und = CM);
+    void setTimeout(unsigned long timeOut) {timeout = timeOut;}
+  };
 }
 #endif
--- src/embedded_common/lib/DistanceSensor.cpp
+++ src/embedded_common/lib/DistanceSensor.cpp
@@ -1,16 +1,112 @@
 // A port of Rob Tillaart's Arduino DHT library to OmniThing
 // https://github.com/RobTillaart/Arduino/tree/master/libraries/DHTstable
-
 #include "distanceSensor.h"
-#include "OmniThing.h"
-#include "Logger.h"
-#include "OmniUtil.h"
-#include "frozen.h"
+
 #include <Arduino.h>
 #include <string.h>
+#include "frozen.h"
+
 
 namespace omni
 {
+//start of DigitalxOutputPinArduino
+  distanceSensor* distanceSensor::createFromJson(const char* json)
+  {
+      unsigned int len = strlen(json);
+
+      unsigned short pin;
+      bool initial;
+      bool invert;
+
+      if(json_scanf(json, len, "{pin: %hu, initial: %B, invert: %B}", &pin, &initial, &invert) != 3)
+      {
+          return nullptr;
+      }
+
+      return new distanceSensor(pin, initial, invert);
+  }
+  void distanceSensor::writePin(bool b)
+  {
+      digitalWrite(getPin(), b);
+  }
+  distanceSensor::distanceSensor(unsigned short pin, bool initialVal, bool invertLogic):
+      DigitalOutputPin(pin, initialVal, invertLogic)
+  {
+      configure();
+      writeBool(initialVal);
+  }
+
+  distanceSensor::~distanceSensor()
+  {
+
+  }
+
+  bool distanceSensor::configure()
+  {
+      pinMode(getPin(), OUTPUT);
+      return true;
+  }
+
+  OutputVoid* distanceSensor::createVoidFromJson(const char* json)
+  {
+      return createFromJson(json);
+  }
+
+  OutputBool* distanceSensor::createBoolFromJson(const char* json)
+  {
+      return createFromJson(json);
+  }
+
+  const char* distanceSensor::Type = "distanceSensor";
+  ObjectConfig<OutputVoid> distanceSensor::OutputVoidConf(Type, createVoidFromJson);
+  ObjectConfig<OutputBool> distanceSensor::OutputBoolConf(Type, createBoolFromJson);
+//end of DigitalOutputPinArduino
+
+//start of DigitalxInputPinArduino
+bool distanceSensor::readPin()
+{
+    return digitalRead(getPin());
+}
+
+//public
+distanceSensor::distanceSensor(unsigned short pin, bool invertLogic, bool pullup):
+    DigitalInputPin(pin, invertLogic),
+    m_bPullup(pullup)
+{
+    configure();
+}
+
+distanceSensor::~distanceSensor()
+{
+
+}
+
+bool distanceSensor::configure()
+{
+    if(m_bPullup)
+        pinMode(getPin(), INPUT_PULLUP);
+    else
+        pinMode(getPin(), INPUT);
+    return true;
+}
+
+InputBool* distanceSensor::createFromJson(const char* json)
+{
+    unsigned int len = strlen(json);
+
+    unsigned short pin;
+    bool invert;
+    bool pullup;
+
+    if(json_scanf(json, len, "{pin: %hu, invert: %B, pullup: %B}", &pin, &invert, &pullup) != 3)
+        return nullptr;
+
+    return new distanceSensor(pin, invert, pullup);
+}
+
+const char* distanceSensor::Type = "distanceSensor";
+ObjectConfig<InputBool> distanceSensor::InputBoolConf(Type, createFromJson);
+//end of DigitalxInputPinArduino
 
 //public from Ultrasonic Library Example
 
